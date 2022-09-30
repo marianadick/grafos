7029f6dc8d4841a9d823796b064a7104edfb4747
Mariana <dickmariana1@gmail.com>
2022-09-30 17:34:55 -0300

    implementacao inicial q1

---
+++ grafo.py
@@ -0,0 +1,80 @@
+class Vertice:
+    def __init__(self, rotulo):
+        self.rotulo = rotulo
+        self.vizinhos = []
+        self.grau = 0
+
+class Aresta:
+    def __init__(self, a, b, peso):
+        self.a = a
+        self.b = b
+        self.peso = peso
+
+class Grafo:
+    def __init__(self):
+        self.vertices = {}
+        self.arestas = {}
+
+    def qtd_vertices(self):  
+        return len(self.vertices.keys())
+
+    def qtd_arestas(self):
+        return len(self.arestas.keys())
+
+    def grau(self, index):
+        return self.vertices[index].grau
+
+    def rotulo(self, index):
+        return self.vertices[index].rotulo
+
+    def vizinhos(self, index):
+        return self.vertices[index].vizinhos
+
+    def ha_aresta(self, a, b):
+        if (self.arestas[a, b]):
+            return True
+        return False
+    
+    def peso(self, a, b):
+        return self.arestas[a, b]
+        
+
+    def ler_arquivo(self, arquivo):   
+        ref_arquivo = open(arquivo, 'r').read().split('\n')
+        lendo_arestas = False
+        
+        for linha in ref_arquivo:
+            if (linha == ''):
+                continue
+
+            if ("*vertices" in linha):
+                continue
+
+            elif ("*edges" in linha):
+                lendo_arestas = True
+                continue
+
+            elif (not lendo_arestas):
+                val = linha.split(' ')
+
+                index = int(val[0])
+                rotulo = val[1]
+                vertice = Vertice(rotulo)
+                self.vertices[index] = vertice
+
+            else:
+                val = linha.split(' ')
+
+                a = self.vertices[int(val[0])]
+                b = self.vertices[int(val[1])]
+                peso = float(val[2])
+                aresta = Aresta(a, b, peso)
+
+                self.arestas[a, b] = aresta
+                a.vizinhos.append(b)
+                a.grau += 1
+                b.vizinhos.append(a)
+                b.grau += 1
+
+grafo = Grafo()
+grafo.ler_arquivo('cor3.net')
