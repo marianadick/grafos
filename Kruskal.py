35fc9e6464f643646f933d58990057232b4e48ac
felipe kettl <fekettl@gmail.com>
2022-11-06 14:32:13 -0300

    Arrumando arquivo

--- Kruskal.py
+++ Kruskal.py
@@ -37,17 +37,11 @@ class Kruskal():
         print(f'{a[len(a)-1][0]}-{a[len(a)-1][1]}')
 
 
-def Programa():
-    nome_do_arquivo = input()
+def Programa(): 
     grafo = Grafo()
+    grafo.ler_arquivo('./testes/agm_tiny.net')
 
-    try:
-        grafo.ler_arquivo(nome_do_arquivo)
-    except:
-        print('Erro com nome de arquivo.')
-        exit(-1)
-
-    Kruskal(grafo).run()
-
+    kruskal = Kruskal(grafo)
+    kruskal.run()
 
 Programa()
